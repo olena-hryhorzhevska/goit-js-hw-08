2e0a8a8a219f34a368f4505106f434a539ac7f22
Olena Hryhorzhevska <elenahry8@gmail.com>
2025-05-24 18:54:02 -0400

    hw done

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const categoriesAmount = document.querySelectorAll(".item");
-console.log(`Number of categories: ${categoriesAmount.length}`);
-categoriesAmount.forEach(el => {
-  const titleText = el.querySelector('h2').textContent;
-  const listAmount = el.querySelectorAll('li').length;
-  console.log(`Category: ${titleText}`);
-  console.log(`Elements: ${listAmount}`);
-})
